0cba3003a6f4dcd4112b6adabbb5d6a68a72f004
Maykopr <maykopr@hotmail.com>
2022-10-31 17:32:02 -0300

    site pronto

---
+++ gallery.js
@@ -0,0 +1,13 @@
+// const FOTOS = document.querySelector(".c-gallery");
+
+// FOTOS.addEventListener('mouseover', loadName);
+
+// function loadName() {
+//     const cityName = document.querySelector('.c-gallery__text');
+//     if (cityName.style.display === 'none') {
+//         cityName.style.display = 'block';
+//     } else {
+//         cityName.style.display = 'none';
+//     }
+
+// }
